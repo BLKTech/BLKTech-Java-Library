4ef235c35db0b4d1c4872183313b7bd4f2124783
blktech <github@blktech.com>
2019-01-31 15:15:38 +0000

    Auto Commit jue ene 31 15:15:38 UTC 2019

---
+++ com/blktech/datatype/Byte.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.blktech.datatype;
+
+import java.nio.ByteBuffer;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Byte {
+    private static ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);    
+
+    public static byte[] longToBytes(long x) {
+        buffer.putLong(0, x);
+        return buffer.array();
+    }
+
+    public static long bytesToLong(byte[] bytes) {
+        buffer.put(bytes, 0, bytes.length);
+        buffer.flip();//need flip 
+        return buffer.getLong();
+    }
+}
