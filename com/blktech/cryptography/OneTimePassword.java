67c64a6e1f97bfc1025a8fab5368fcd1816b99bf
blktech <github@blktech.com>
2019-01-31 17:01:17 +0000

    Auto Commit jue ene 31 17:01:17 UTC 2019

--- com/blktech/cryptography/OneTimePassword.java
+++ com/blktech/cryptography/OneTimePassword.java
@@ -5,7 +5,6 @@
  */
 package com.blktech.cryptography;
 
-import java.security.NoSuchAlgorithmException;
 
 /**
  *
@@ -13,141 +12,100 @@ import java.security.NoSuchAlgorithmException;
  */
 public class OneTimePassword 
 {
- 
-    private Hash hashAlgorithm;
+    private static byte[] combineBytes(byte[] passwordHash, byte[] timeHash)
+    {
+        byte result[] = new byte[passwordHash.length + timeHash.length + passwordHash.length];        
+            
+        System.arraycopy(passwordHash,  0, result,  0,                                      passwordHash.length);
+        System.arraycopy(timeHash,      0, result,  passwordHash.length,                    timeHash.length);
+        System.arraycopy(passwordHash,  0, result,  passwordHash.length + timeHash.length,  passwordHash.length);
+        
+        return result;
+    }
+    
+    
+    private Hash hashAlgorithm;    
+    private byte[] passwordHash;
+    
+    private int timeDivider;       
     private long timeOffset;
-    private int timeDivider;
-
-    public OneTimePassword(Hash hashAlgorithm, long timeOffset, int timeDivider) 
+    private long timeCurrent;
+    
+    private byte[] currentPassword;
+    
+    
+    
+    public OneTimePassword(Hash hashAlgorithm, int timeDivider, String password, long timeOffset) 
+    {
+        this(hashAlgorithm, timeDivider, password.getBytes(), timeOffset);
+    }
+    public OneTimePassword(Hash hashAlgorithm, int timeDivider, byte[] password, long timeOffset) 
     {
         this.hashAlgorithm = hashAlgorithm;
-        this.timeOffset = timeOffset;
         this.timeDivider = timeDivider;
+        this.passwordHash = hashAlgorithm.calc(password);
+        this.timeOffset = timeOffset;
     }
 
-    public OneTimePassword(Hash hashAlgorithm, long timeOffset) 
-    {
-        this(hashAlgorithm, timeOffset, 60);
-    }
-
-    public OneTimePassword(Hash hashAlgorithm, int timeDivider) 
-    {
-        this(hashAlgorithm, 0, timeDivider);
-    }
-
-    public OneTimePassword(Hash hashAlgorithm) 
-    {
-        this(hashAlgorithm, 0, 60);
-    }
-
-    public OneTimePassword(long timeOffset, int timeDivider) throws NoSuchAlgorithmException 
-    {
-        this(Hash.getAlgorithm("SHA-256"), timeOffset, timeDivider);
-    }
-
-    public OneTimePassword(long timeOffset) throws NoSuchAlgorithmException 
-    {
-        this(timeOffset, 60);
-    }
-
-    public OneTimePassword(int timeDivider) throws NoSuchAlgorithmException 
-    {
-        this(0, timeDivider);
-    }
+    
     
     public long getTimeOffset() 
     {
         return timeOffset;
-    }
-
+    }    
     public void setTimeOffset(long timeOffset) 
     {
-        this.timeOffset = timeOffset;
+        if(this.timeOffset != timeOffset)
+        {
+            this.timeOffset = timeOffset;
+            this.currentPassword = new byte[0];
+        }
+    }
+    private long getTime() 
+    {
+        return  System.currentTimeMillis() / 1000L + this.timeOffset;
     }
 
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-   private static long getTime(long timeOffset) 
+    public int getTimeDivider() 
     {
-        return  System.currentTimeMillis() / 1000L + timeOffset;
+        return timeDivider;
     }
-    
-    private static long getSubTime(long timeOffset,int timeDivider)
+    public void setTimeDivider(int timeDivider) 
     {
-        return getTime(timeOffset) / timeDivider;
+        if(this.timeDivider != timeDivider)
+        {
+            this.timeDivider = timeDivider;
+            this.currentPassword = new byte[0];
+        }
+    }
+    private long getSubTime()
+    {
+        return getTime() / this.timeDivider;
     }    
     
-    private static byte[] getSubTimeBytes(long timeOffset,int timeDivider)
+        
+        
+    
+    public byte[] getOneTimePassword(long timeOffset)
     {
-        return com.blktech.datatype.Byte.fromLong(getSubTime(timeOffset, timeDivider));
+        this.setTimeOffset(timeOffset);
+        return getOneTimePassword();
+    }    
+    private byte[] getOneTimePassword() 
+    {                
+        long timeCurrent = this.getSubTime();
+        
+        if(this.currentPassword.length==0 || this.timeCurrent != timeCurrent)
+        {
+            this.timeCurrent = timeCurrent;                    
+            byte[] timeHash = this.hashAlgorithm.calc(com.blktech.datatype.Byte.fromLong(this.timeCurrent));                                    
+            this.currentPassword = this.hashAlgorithm.calc(combineBytes(this.passwordHash, timeHash));
+        }
+
+        return this.currentPassword;
     }    
     
-    private static byte[] calcHash(Hash hash, byte[] data)
-    {
-        return hash.calc(data);
-    }
-        
-    
-    
-    private byte[] getPasswordHash(byte[] password)
-    {
-        return this.hashAlgorithm.calc(password);
-    }
-    
-    private byte[] getPasswordHash(String password)
-    {
-        return this.getPasswordHash(password.getBytes());
-    }    
-    
-    public byte[] calc(String password, long timeOffset, int timeDivider)
-    {
-        return this.calc(password.getBytes(), long timeOffset, int timeDivider);
-    }
-    
-    public byte[] calc(byte[] password, long timeOffset, int timeDivider)
-    {
-        byte[] passwordHash = calcHash(this.hashAlgorithm, password);
-        byte[] timeHash = calcHash(this.hashAlgorithm, getSubTimeBytes(timeOffset, timeDivider));
-        byte[] result = new byte[passwordHash.length+timeHash.length+passwordHash.length];
-        
-        System.arraycopy(passwordHash, 0, result, 0, passwordHash.length);
-        System.arraycopy(timeHash, 0, result, passwordHash.length, timeHash.length);
-        System.arraycopy(passwordHash, 0, result, passwordHash.length + timeHash.length, passwordHash.length);          
-        
-        return calcHash(this.hashAlgorithm,  result);
-    }
 
-    
-//    private byte[] getTimePassword(byte[] password) 
-//    {
-//        byte[] time = com.blktech.datatype.Byte.fromLong(this.getTime()/60);
-//        byte[] result = new byte[password.length+time.length+password.length];
-//        System.arraycopy(password, 0, result, 0, password.length);
-//        System.arraycopy(time, 0, result, password.length, time.length);
-//        System.arraycopy(password, 0, result, password.length + time.length, password.length);        
-//        return Hash.getAlgorithm("SHA-256").calc(result);
-//    }
-//    
-
-
-    
-    
 }
